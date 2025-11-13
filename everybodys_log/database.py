dd9386d7ed7be4ba9583a4f259fa47a6949d4777
raiymbekzxc <raiymbek.zhumatov05@gmail.com>
2025-11-13 11:46:41 +0500

    Added database connection

--- everybodys_log/database.py
+++ everybodys_log/database.py
@@ -4,11 +4,11 @@ from dotenv import load_dotenv
 
 load_dotenv() #.env file
 
-USER = os.getenv("user")
-PASSWORD = os.getenv("password")
-HOST = os.getenv("host")
-PORT = os.getenv("port")
-DBNAME = os.getenv("dbname")
+USER = os.getenv("USER")
+PASSWORD = os.getenv("PASSWORD")
+HOST = os.getenv("HOST")
+PORT = os.getenv("PORT")
+DBNAME = os.getenv("DBNAME")
 
 DATABASE_URL = f"postgresql+psycopg2://{USER}:{PASSWORD}@{HOST}:{PORT}/{DBNAME}?sslmode=require"
 engine = create_engine(DATABASE_URL, echo=True)
