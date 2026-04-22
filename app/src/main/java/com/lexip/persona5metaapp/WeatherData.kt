01ea587470d7c834c462ec894462281815d3c6a7
Alidar <alidar.kassymzhan@gmail.com>
2026-04-23 00:37:56 +0500

    moon icon

--- app/src/main/java/com/lexip/persona5metaapp/WeatherData.kt
+++ app/src/main/java/com/lexip/persona5metaapp/WeatherData.kt
@@ -1,9 +1,12 @@
 package com.lexip.persona5metaapp
 
+import com.google.gson.annotations.SerializedName
+
 data class WeatherResponse(
     val current: CurrentWeather?
 )
 
 data class CurrentWeather(
-    val weather_code: Int
+    val weather_code: Int,
+    val is_day: Int
 )
