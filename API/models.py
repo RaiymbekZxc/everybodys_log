b71820fcb3ae330c04226b1200305d57e4982eb5
RaiymbekZxc <raiymbek.zhumatov05@gmail.com>
2026-04-30 15:38:41 +0500

    JWT logic

--- API/models.py
+++ API/models.py
@@ -1,10 +1,30 @@
 
 
 from sqlmodel import SQLModel, Field
+from pydantic import BaseModel 
+from pwdlib import PasswordHash
+from .auth import DUMMY_HASH
+
+class Token(BaseModel):
+    access_token: str
+    token_type: str
+
+class TokenData(BaseModel):
+    username: str | None = None
+
+class User(BaseModel):
+    username: str
+    name: str
+    email: str
+    disabled: bool | None = None
+
+class UserInDB(User):
+    hashed_password: str
 
 
-class User(SQLModel, table=True):
+class UserTable(SQLModel, table=True):
     username: str = Field(default=None, primary_key=True)
     name: str = Field(index=True)
     email: str = Field(index=True)
-    password: str
+    hashed_password: str = Field(default=DUMMY_HASH)
+
