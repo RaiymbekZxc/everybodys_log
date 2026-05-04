e58789d1963591b065ea6afb635d4f48e42274e1
Alidar <alidar.kassymzhan@gmail.com>
2026-05-04 15:10:45 +0500

    activity log widget no longer clickable, for now at least

--- app/src/main/java/com/lexip/persona5metaapp/ActivityLogWidgetProvider.kt
+++ app/src/main/java/com/lexip/persona5metaapp/ActivityLogWidgetProvider.kt
@@ -1,8 +1,10 @@
 package com.lexip.persona5metaapp
 
+import android.app.PendingIntent
 import android.appwidget.AppWidgetManager
 import android.appwidget.AppWidgetProvider
 import android.content.Context
+import android.content.Intent
 import android.util.Log
 import android.widget.RemoteViews
 
@@ -24,5 +26,17 @@ class ActivityLogWidgetProvider : AppWidgetProvider() {
         appWidgetId: Int
     ){
         val views = RemoteViews(context.packageName, R.layout.activity_log_widget)
+
+        val emptyIntent = Intent().apply{
+            action = "com.lexip.persona5metaapp.EMPTY_ACTION"
+        }
+        val pendingIntent = PendingIntent.getBroadcast(
+            context,
+            0,
+            emptyIntent,
+            PendingIntent.FLAG_IMMUTABLE
+        )
+        views.setOnClickPendingIntent(R.id.activity_log_root, pendingIntent)
+        appWidgetManager.updateAppWidget(appWidgetId, views)
     }
 }
