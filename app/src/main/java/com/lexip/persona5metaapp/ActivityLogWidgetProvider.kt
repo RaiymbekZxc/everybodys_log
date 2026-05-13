58169009131d3db1e7392a7cf8799f619b943492
Alidar <alidar.kassymzhan@gmail.com>
2026-05-13 20:47:28 +0500

    percentage visualisation

--- app/src/main/java/com/lexip/persona5metaapp/ActivityLogWidgetProvider.kt
+++ app/src/main/java/com/lexip/persona5metaapp/ActivityLogWidgetProvider.kt
@@ -37,6 +37,27 @@ class ActivityLogWidgetProvider : AppWidgetProvider() {
             PendingIntent.FLAG_IMMUTABLE
         )
         views.setOnClickPendingIntent(R.id.activity_log_root, pendingIntent)
+
+        // Обновление процентов
+        var productivity = 69
+        var socialLife = 68
+        var leisure = 67
+
+        val currentProductivityPercent = "activity_log_productivity_${productivity}"
+        val productivityResId = context.resources.getIdentifier(currentProductivityPercent, "drawable",context.packageName)
+
+        val currentSocialLifePercent = "activity_log_sociallife_${socialLife}"
+        val socialLifeResId = context.resources.getIdentifier(currentSocialLifePercent, "drawable",context.packageName)
+
+        val currentLeisurePercent = "activity_log_leisure_${leisure}"
+        val leisureResId = context.resources.getIdentifier(currentLeisurePercent, "drawable",context.packageName)
+
+        if (productivityResId != 0 && socialLifeResId != 0 && leisureResId != 0){
+            views.setImageViewResource(R.id.productivity_percent, productivityResId)
+            views.setImageViewResource(R.id.social_life_percent, socialLifeResId)
+            views.setImageViewResource(R.id.leisure_percent, leisureResId)
+        }
+
         appWidgetManager.updateAppWidget(appWidgetId, views)
     }
 }
