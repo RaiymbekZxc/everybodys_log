038daab55749e088fad2f15c32a75eb1ff9f314b
Alidar <alidar.kassymzhan@gmail.com>
2026-05-15 20:47:58 +0500

    style

--- app/src/main/java/com/lexip/persona5metaapp/ActivityLogConfig.kt
+++ app/src/main/java/com/lexip/persona5metaapp/ActivityLogConfig.kt
@@ -10,11 +10,6 @@ class ActivityLogConfig : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_log_config)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
 
         val webView = findViewById<WebView>(R.id.webView)
         webView.settings.apply {
